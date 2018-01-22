ee5385025599b8611657ab3bed240560d6caf78c
Adelina Enache <enache.adelina99@gmail.com>
2018-01-22 17:03:09 +0200

    Add skipmixin

--- operators/freshsales_to_s3_operator.py
+++ operators/freshsales_to_s3_operator.py
@@ -1,9 +1,10 @@
 from airflow.hooks.S3_hook import S3Hook
-from airflow.models import BaseOperator
+from airflow.models import BaseOperator, SkipMixin
 from airflow.hooks.http_hook import HttpHook
 from tempfile import NamedTemporaryFile
 from dateutil.parser import parse
 import json
+import logging
 
 class EndpointNotSupported(Exception):
 
@@ -11,7 +12,7 @@ class EndpointNotSupported(Exception):
         super().__init__("Specified endpoint not currently supported.")
 
 
-class FreshsalesToS3Operator(BaseOperator):
+class FreshsalesToS3Operator(BaseOperator, SkipMixin):
     """
     Trello to S3 Operator
     :param freshsales_conn_id:      The Airflow id used to store the Freshsales
@@ -36,7 +37,7 @@ class FreshsalesToS3Operator(BaseOperator):
     :type s3_bucket:                string
     :param updated_at:              replication key
     :type:                          datetime
-    """ 
+    """
 
     def __init__(self,
                  freshsales_conn_id,
@@ -58,8 +59,8 @@ class FreshsalesToS3Operator(BaseOperator):
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
 
-    def filter_result(self, result): 
-        if (not self.updated_at) or ('updated_at' not in result): 
+    def filter_result(self, result):
+        if (not self.updated_at) or ('updated_at' not in result):
             return True
         return self.updated_at < parse(result['updated_at'])
 
@@ -69,7 +70,7 @@ class FreshsalesToS3Operator(BaseOperator):
         """
         results = hook.run(
             "{}/filters".format(endpoint)).json()['filters']
-        
+
         return [result['id'] for result in results if self.filter_result(result)]
 
     def get_by_filters(self, hook, endpoint):
@@ -82,7 +83,7 @@ class FreshsalesToS3Operator(BaseOperator):
         for view_id in ids:
             response = hook.run(
                 '{}/view/{}'.format(endpoint, view_id)).json()
-            
+
             results.extend(response[endpoint])
 
         return results
@@ -90,8 +91,9 @@ class FreshsalesToS3Operator(BaseOperator):
     def execute(self, context):
         hook = HttpHook(method='GET', http_conn_id=self.freshsales_conn_id)
 
-        if self.freshsales_endpoint == 'tasks': 
-            task_filters = ['open', 'due%20today', 'due%20tomorrow', 'overdue', 'completed']
+        if self.freshsales_endpoint == 'tasks':
+            task_filters = ['open', 'due%20today',
+                            'due%20tomorrow', 'overdue', 'completed']
             results = []
 
             for task_filter in task_filters:
@@ -100,25 +102,40 @@ class FreshsalesToS3Operator(BaseOperator):
                 results.extend(response['tasks'])
         else:
             results = self.get_by_filters(hook, self.freshsales_endpoint)
-        
+
         results = filter(lambda x: self.filter_result(x), results)
 
-        for result in results:
-            print(result)
+        if len(results) == 0 or results is None:
+            logging.info("No records pulled from Trello.")
+            downstream_tasks = context['task'].get_flat_relatives(
+                upstream=False)
+            logging.info('Skipping downstream tasks...')
+            logging.debug("Downstream task_ids %s", downstream_tasks)
 
-        # with NamedTemporaryFile("w") as tmp:
-        #     for result in results:
-        #         tmp.write(json.dumps(result) + '\n')
+            if downstream_tasks:
+                self.skip(context['dag_run'],
+                          context['ti'].execution_date,
+                          downstream_tasks)
+            return True
 
-        #     tmp.flush()
+        else:
+            # Write the results to a temporary file and save that file to s3.
+            with NamedTemporaryFile("w") as tmp:
+                for result in results:
+                    filtered_result = self.filter_fields(result)
+                    tmp.write(json.dumps(filtered_result) + '\n')
 
-        #     dest_s3 = S3Hook(s3_conn_id=self.s3_conn_id)
-        #     dest_s3.load_file(
-        #         filename=tmp.name,
-        #         key=self.s3_key,
-        #         bucket_name=self.s3_bucket,
-        #         replace=True
+                tmp.flush()
 
-        #     )
-        #     dest_s3.connection.close()
-        #     tmp.close()
+                dest_s3 = S3Hook(s3_conn_id=self.s3_conn_id)
+                dest_s3.load_file(
+                    filename=tmp.name,
+                    key=self.s3_key,
+                    bucket_name=self.s3_bucket,
+                    replace=True
+
+                )
+                dest_s3.connection.close()
+                tmp.close()
+
+        
