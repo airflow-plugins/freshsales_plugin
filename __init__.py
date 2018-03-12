eec19d94b528c48fd09fdc64716470ed6133da89
Benjamin Gregory <benjaminrgregory@gmail.com>
2018-03-12 12:18:46 -0600

    import salesforcehook

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from airflow.plugins_manager import AirflowPlugin
 
+from freshsales_plugin.hooks.freshsales_hook import FreshSalesHook
 from freshsales_plugin.operators.freshsales_to_s3_operator import FreshsalesToS3Operator
 
 
 class freshsales_plugin(AirflowPlugin):
     name = "freshsales_plugin"
     operators = [FreshsalesToS3Operator]
-    hooks = []
+    hooks = [FreshSalesHook]
     # Leave in for explicitness
     executors = []
     macros = []
